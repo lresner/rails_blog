5dbe42218a4be62ea516058e762a0c3e36eb6dee
dbc-apprentice <apprentice@dbc-chicago-008.local>
2013-09-15 17:42:34 -0500

    cr blog

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyFirstRailsApp::Application.config.secret_token = '01b15ab2a496e17e00ebd5485a144f285518b366504ce109bcc3f84c9e9a5b55d2a288c60ae4d79b573305a4142780d8fa5d616c03465c2c40d8fcdca9ffa6cb'
